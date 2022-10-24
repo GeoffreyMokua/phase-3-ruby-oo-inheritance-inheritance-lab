5af73ba3048865d180299be08b4bdfe7ba61fe93
GeoffreyMokua <geoffrey.nyakundi@student.moringaschool.com>
2022-10-24 10:40:25 +0300

    completed assignment

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,22 @@
-class Student 
+class Student < User
+    @knowledge = []
+    attr_reader :knowledge
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(string)
+        @knowledge << string
+    end
+
+    def knowledge
+        @knowledge
+    end
+
+    def learn(knowledge)
+        @knowledge << knowledge
+    end
+
 
 end
